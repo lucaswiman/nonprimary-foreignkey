00dac5f9119dcba343d3b8ad88cf930dca63d05e
Lucas Wiman <lucaswiman@counsyl.com>
2016-04-05 14:18:55 -0700

    Add failing test for object prefetching

--- nonprimary_foreignkey/tests/tests.py
+++ nonprimary_foreignkey/tests/tests.py
@@ -53,3 +53,28 @@ class TestSet(TestCase):
         from_instance = ReceivedItem.objects.create()
         self.assertEqual(from_instance.barcode, None)
         self.assertEqual(from_instance.item, None)
+
+
+class TestPrefetch(TestCase):
+    def setUp(self):
+        Item.objects.all().delete()
+        self.barcode1 = '12345'
+        self.item1 = Item.objects.create(barcode=self.barcode1)
+        self.barcode2 = '67890'
+        self.item2 = Item.objects.create(barcode=self.barcode2)
+
+    def test_constant_queries(self):
+        self.assertEqual(
+            ReceivedItem.objects.create(barcode=self.barcode1).item,
+            self.item1)
+        self.assertEqual(
+            ReceivedItem.objects.create(barcode=self.barcode2).item,
+            self.item2)
+        with self.assertNumQueries(1):
+            self.assertEqual(len(ReceivedItem.objects.all()), 2)
+        # Prefetching should include the query to grab the related ``Item`` objects.
+        with self.assertNumQueries(2):
+            queryset = ReceivedItem.objects.prefetch_related('item')
+            self.assertEqual(len(queryset), 2)
+        with self.assertNumQueries(0):
+            self.assertEqual({obj.item for obj in queryset}, {self.item1, self.item2})
